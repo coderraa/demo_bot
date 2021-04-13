c9f2605955e60afac1255c5b1208adfcbaa0b267
Rahul Singh <57325166+coderraa@users.noreply.github.com>
2021-04-14 01:16:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const express = require('express')
 const app = express()
+const nodemailer = require("nodemailer");
 const axios = require('axios');
 let port = process.env.PORT || 3000
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+      user: 'cokeaglapp@gmail.com',
+      pass: 'Cola@2019Dev'
+  }
+});
+
+
 
 app.use(express.json());
 
@@ -69,6 +80,60 @@ app.post('/', (req, res) => {
         ]
       })
     })}
+  
+  else if(req.body.queryResult.intent.displayName=='Others mail trigger'){
+    nextbilldue().then(function(resp) {
+      console.log(resp)//resp is reponse from get api, 
+      const mailOptions = {
+        from: "cokeaglapp@gmail.com", // sender address
+        to: "rahul.singh.ds20@gmail.com", // list of receivers
+        subject: "Customer Complain", // Subject line
+        html: req.body.queryResult.queryText
+      };
+      transporter.sendMail(mailOptions, function (err, info) {
+        if(err)
+        {
+          console.log(err);
+        }
+    });
+      //res.send will send this to dialogflow
+      res.send({
+        "fulfillmentMessages": [
+          {
+            "text": {
+              "text": ["Your query: "+req.body.queryResult.queryText+" has been registered. We will Contact you soon. Thank You"]
+            }
+          }
+        ]
+      })
+    })}
+
+    else if(req.body.queryResult.intent.displayName=='Others Cooler mail trigger'){
+      nextbilldue().then(function(resp) {
+        console.log(resp)//resp is reponse from get api, 
+        const mailOptions = {
+          from: "cokeaglapp@gmail.com", // sender address
+          to: "rahul.singh.ds20@gmail.com", // list of receivers
+          subject: "Cooler Customer Complain", // Subject line
+          html: req.body.queryResult.queryText
+        };
+        transporter.sendMail(mailOptions, function (err, info) {
+          if(err)
+          {
+            console.log(err);
+          }
+      });
+        //res.send will send this to dialogflow
+        res.send({
+          "fulfillmentMessages": [
+            {
+              "text": {
+                "text": ["Your Cooler Complain query: "+req.body.queryResult.queryText+" has been registered. We will Contact you soon. Thank You"]
+              }
+            }
+          ]
+        })
+      })}
 
     else if(req.body.queryResult.parameters.Trigger_entity=='SOA'){
       soa().then(function(resp) {
