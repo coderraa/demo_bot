99014f69375ec684956ccf7b8ada6e5b2ab243e7
Rahul Singh <57325166+coderraa@users.noreply.github.com>
2021-05-05 02:11:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -131,7 +131,7 @@ app.post('/', (req, res) => {
           "fulfillmentMessages": [
             {
               "text": {
-                "text": [JSON.stringify(resp)+" Your Cooler Complain query: --"+req.body.queryResult.queryText+"-- has been registered. We will Contact you soon."]
+                "text": [JSON.stringify(resp)+" Your Cooler Complain query: --"+req.body.queryResult.parameters.Trigger_entity+"-- has been registered. We will Contact you soon."]
               }
             }
           ]
@@ -146,7 +146,7 @@ app.post('/', (req, res) => {
             "fulfillmentMessages": [
               {
                 "text": {
-                  "text": [JSON.stringify(resp)+" Your Cooler Complain query: --"+req.body.queryResult.queryText+"-- has been registered. We will Contact you soon."]
+                  "text": [JSON.stringify(resp)+" Your Cooler Complain query: --"+req.body.queryResult.parameters.Trigger_entity+"-- has been registered. We will Contact you soon."]
                 }
               }
             ]
@@ -160,7 +160,7 @@ app.post('/', (req, res) => {
               "fulfillmentMessages": [
                 {
                   "text": {
-                    "text": [JSON.stringify(resp)+" Your Cooler Complain query: --"+req.body.queryResult.queryText+"-- has been registered. We will Contact you soon."]
+                    "text": [JSON.stringify(resp)+" Your Cooler Complain query: --"+req.body.queryResult.parameters.Trigger_entity+"-- has been registered. We will Contact you soon."]
                   }
                 }
               ]
